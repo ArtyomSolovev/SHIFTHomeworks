1e5a6314ae660619d3ba824e3d6a944653a3d7b4
ArtyomSolovev <artyom.solovev@icloud.com>
2021-11-05 22:18:15 +0300

    Доделал функционал приложения

--- ThreadSafeArray/ThreadSafeArray/main.swift
+++ ThreadSafeArray/ThreadSafeArray/main.swift
@@ -11,6 +11,7 @@ struct ThreadSafeArray<T> {
     
     private var array = Array<T>()
     private let queue = DispatchQueue(label: "SafeQueue", qos: .default, attributes: .concurrent)
+    let semaphore = DispatchSemaphore(value: 1)
     
     init(_ addingArray: [T]){
         array = addingArray
@@ -31,14 +32,19 @@ struct ThreadSafeArray<T> {
     
     mutating func append(_ item: T){
         queue.sync {
+            semaphore.wait()
             array.append(item)
+            semaphore.signal()
         }
     }
     mutating func remove(at index: Int) {
         queue.sync {
+            semaphore.wait()
             array.remove(at: index)
+            semaphore.signal()
         }
     }
+    
     subscript (index: Int) -> T{
         get{
             queue.sync {
@@ -46,45 +52,25 @@ struct ThreadSafeArray<T> {
             }
         }
     }
-//    array.contains(<#T##Self.Element#>)
-//    func contains(_ element: T) -> Bool where T: Sequence{
-//
-//        var elements: Array<T>?
-//
-//        return array.contains{ $0 == element}
-//        for index in array{
-//            if index == element {
-//                return true
-//            }
-//        }
-//        return false
-//
-//    }
+    func contains(_ element: T) -> Bool where T:Equatable {
+        return array.contains{ $0 == element}
+    }
     
-//    func isEmpty() -> Bool {
-//        array.isEmpty
-//    }
 }
 
-//extension ThreadSafeArray: Equatable{
-//    static func == (lhs: ThreadSafeArray<T>, rhs: ThreadSafeArray<T>) -> Bool {
-//        lhs == rhs
-//    }
-//}
+var safeArray = ThreadSafeArray([])
 
-var a = ThreadSafeArray([])
+let thread = DispatchQueue(label: "No", attributes: .concurrent)
 
-let aa = DispatchQueue(label: "No", attributes: .concurrent)
-
-aa.async {
-    for number in 0...100{
-        print(number)
-        a.append(number)
-    }
-}
-aa.async {
+thread.async {
     for number in 0...1000{
-        a.append(number)
+        safeArray.append(number)
     }
 }
-print(a.count)
+thread.async {
+    for number in 0...1000{
+        safeArray.append(number)
+    }
+}
+sleep(1)
+print("Result:",safeArray.count)
